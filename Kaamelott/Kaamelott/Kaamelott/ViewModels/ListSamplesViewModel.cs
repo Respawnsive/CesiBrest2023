3e8552a611312ed4b214a78ec9ea8b06479b5f69
Jérôme Liger <jerome.liger@respawnsive.com>
2023-02-09 16:54:02 +0100

    ListSamplePage terminée (ou presque)

--- Kaamelott/Kaamelott/Kaamelott/ViewModels/ListSamplesViewModel.cs
+++ Kaamelott/Kaamelott/Kaamelott/ViewModels/ListSamplesViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Kaamelott.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Kaamelott.ViewModels
 {
@@ -9,10 +13,59 @@ namespace Kaamelott.ViewModels
     {
         public ListSamplesViewModel()
         {
+            ListSaample = new ObservableCollection<Saample>();
+            ListSaample.Add(new Saample
+            {
+                Title = "Interprète",
+                Character = "Arthur - Le Roi Burgonde",
+                Episode = "Livre II, 03 - Le Dialogue de Paix",
+                File = "interprete.mp3",
+                Imagefile = "arthurleroiburgonde.png"
+            });
+            ListSaample.Add(new Saample
+            {
+                Title = "JE NE MANGE PAS DE GRAINES",
+                Character = "Le Maître d’armes",
+                Episode = "Livre II, 26 - Corpore sano",
+                File = "je_ne_mange_pas_de_graines.mp3",
+                Imagefile = "lemaitredarmes.png"
+            });
+
+            ClickSaampleCommand = new Command(ExecuteSaample, CanExecuteSaample);
+        }
+
+        private ObservableCollection<Saample> listSaample;
+        public ObservableCollection<Saample> ListSaample
+        {
+            get
+            {
+                return listSaample;
+            }
+            set
+            {
+                listSaample = value;
+            }
+        }
+
+        private Saample selectedSaample;
+        public Saample SelectedSaample
+        {
+            get { return selectedSaample; }
+            set { selectedSaample = value; }
+        }
+
+
+        public ICommand ClickSaampleCommand { get; set; }
+
+        private void ExecuteSaample()
+        {
+            //Logique
 
         }
 
-        public List<Saample> ListSaample { get; set; }
-
+        private bool CanExecuteSaample()
+        {
+            return SelectedSaample != null;
+        }
     }
 }
