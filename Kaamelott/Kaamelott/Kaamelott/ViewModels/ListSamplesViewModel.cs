fa9818679f85ca787740ad569ed17c97016ed435
Jérôme Liger <jerome.liger@respawnsive.com>
2023-02-09 15:35:35 +0100

    ListsamplesPage BindingContext implemented

---
+++ Kaamelott/Kaamelott/Kaamelott/ViewModels/ListSamplesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Kaamelott.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaamelott.ViewModels
+{
+    public  class ListSamplesViewModel
+    {
+        public ListSamplesViewModel()
+        {
+
+        }
+
+        public List<Saample> ListSaample { get; set; }
+
+    }
+}
