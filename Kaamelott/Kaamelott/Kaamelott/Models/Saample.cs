4657b02b0864a3e0ea5119fbdb622a362c3346f0
Jérôme Liger <jerome.liger@respawnsive.com>
2023-02-09 15:00:49 +0100

    Ajout ObjetSaample et Listpage

---
+++ Kaamelott/Kaamelott/Kaamelott/Models/Saample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Kaamelott.Models
+{
+    public class Saample
+    {
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("character")]
+        public string Character { get; set; }
+
+        [JsonPropertyName("episode")]
+        public string Episode { get; set; }
+
+        [JsonPropertyName("file")]
+        public string File { get; set; }
+
+        [JsonPropertyName("imagefile")]
+        public string Imagefile { get; set; }
+
+
+    }
+}
